9e2b21de6c86a0c6388a4677eee0f026b95ffd98
Sriraman <sriramancse@gmail.com>
2018-04-18 23:39:49 +0530

    Fix the translate in tabbedform

--- src/mui/form/TabbedForm.js
+++ src/mui/form/TabbedForm.js
@@ -68,9 +68,7 @@ export class TabbedForm extends Component {
                                     <Tab
                                         key={tab.props.label}
                                         className="form-tab"
-                                        label={translate(tab.props.label, {
-                                            _: tab.props.label,
-                                        })}
+                                        label={tab.props.label}
                                         value={index}
                                         icon={tab.props.icon}
                                         buttonStyle={
